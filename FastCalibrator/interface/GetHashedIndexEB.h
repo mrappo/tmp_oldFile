eb84d34c2cdc3acdbda4903d5d850e25ecf6e1a4
rgerosa <>
2011-11-19 11:41:59 +0000

    Hashed Index info

---
+++ FastCalibrator/interface/GetHashedIndexEB.h
@@ -0,0 +1,23 @@
+
+#include <TROOT.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <TH1F.h>
+#include <TProfile.h>
+#include <vector>
+#include "hChain.h"
+#include "h2Chain.h"
+#include <TGraphErrors.h>
+
+#include <TLorentzVector.h>
+#include "Math/PtEtaPhiE4D.h"
+#include "Math/PtEtaPhiM4D.h"
+#include "Math/LorentzVector.h"
+
+
+
+int GetHashedIndexEB(int iEta, int iPhi, int Zside);
+
+int GetIetaFromHashedIndex(int Index);
+
+int GetIphiFromHashedIndex(int Index);
