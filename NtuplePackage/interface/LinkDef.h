9d815c6ccac371501dd52516662f7fbafa678ae9
abenagli <>
2012-08-23 19:14:51 +0000

    updates

--- NtuplePackage/interface/LinkDef.h
+++ NtuplePackage/interface/LinkDef.h
@@ -12,7 +12,6 @@
 #pragma link C++ function deltaEta (const double&, const double&);
 #pragma link C++ function deltaPhi (const double&, const double&);
 #pragma link C++ function deltaR (const double&, const double&, const double&, const double&);
-#pragma link C++ function PURescaleFactor (const int&);
 
 #endif
 
