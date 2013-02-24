ebba91ddaf423fd7bb946f838ce744a34e3c5ca3
rgerosa <>
2013-02-24 17:02:38 +0000

    Resolution Class for Kinematic Fit

---
+++ VBFHWWlnuJ/interface/Resolution.h
@@ -0,0 +1,30 @@
+#ifndef Resolution_h
+#define Resolution_h
+
+class Resolution {
+
+public:
+
+  Resolution();
+
+  virtual ~Resolution();
+
+  bool electronResolution(const double et, const double eta,
+                          double& etRes, double& etaRes, double& phiRes);
+
+  bool muonResolution(const double et, const double eta,
+                      double& etRes, double& etaRes, double& phiRes);
+
+  bool PFMETResolution(const double et, double& etRes, double& etaRes, double& phiRes);
+
+  bool udscPFJetResolution(const double et, const double eta,
+                           double& etRes, double& etaRes, double& phiRes);
+
+  bool bPFJetResolution(const double et, const double eta,
+                        double& etRes, double& etaRes, double& phiRes);
+
+  inline double square(const double x) {return x*x;}
+
+};  
+
+#endif
