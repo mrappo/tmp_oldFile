69b5c5420e0e14bddd34cdfc94b1414dd1c00c3d
rgerosa <>
2013-02-24 17:01:54 +0000

    Last Version

--- VBFHWWlnuJ/scripts/setup.sh
+++ VBFHWWlnuJ/scripts/setup.sh
@@ -2,6 +2,8 @@ export THISDIR=`pwd`
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/lib
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/NtuplePackage/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/../KinFitter/lib
+
 # For CLHEP
 export CLHEPSYS=/afs/cern.ch/sw/lcg/external/clhep/2.1.3.1/x86_64-slc5-gcc46-opt/
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CLHEPSYS}/lib
@@ -9,6 +11,7 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${CLHEPSYS}/lib
 if [ -n "${DYLD_LIBRARY_PATH}" ] ; then
 export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${THISDIR}/lib
 export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${THISDIR}/NtuplePackage/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/../KinFitter/lib
 fi
 
 
@@ -16,8 +19,13 @@ fi
 export PATH=${PATH}:${THISDIR}/bin
 export PATH=${PATH}:${CLHEPSYS}/bin
 export PATH=${PATH}:${THISDIR}/NtuplePackage/bin
+export PATH=${PATH}:${THISDIR}/../KinFitter/bin
+
 
 export NTUPLEPKGINCLUDE=${THISDIR}/NtuplePackage/interface
 export NTUPLEPKGLIB=${THISDIR}/NtuplePackage/lib
+
+export KINFITPKGINCLUDE=${THISDIR}/../KinFitter/interface
+export KINFITPKGLIB=${THISDIR}/../KinFitter/lib
 
 export VBFHWWlnJ=${THISDIR}/
