0a5f1c82682ebf1851fa94d0c5d06462c30150b1
rgerosa <>
2013-02-25 09:36:42 +0000

    First Import

---
+++ VBFHWWlnuJ/KinFitter/scripts/setup.csh
@@ -0,0 +1,12 @@
+setenv THISDIR `pwd`
+
+setenv LD_LIBRARY_PATH ${LD_LIBRARY_PATH}:${THISDIR}/lib
+
+if (${?DYLD_LIBRARY_PATH}) then
+setenv DYLD_LIBRARY_PATH ${DYLD_LIBRARY_PATH}:${THISDIR}/lib
+endif
+
+setenv PATH ${PATH}:${THISDIR}/bin
+
+setenv KINFITPKGINCLUDE ${THISDIR}/interface
+setenv KINFITPKGLIB ${THISDIR}/lib
---
+++ VBFHWWlnuJ/KinFitter/scripts/setup.sh
@@ -0,0 +1,12 @@
+export THISDIR=`pwd`
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/lib
+
+if [ -n "${DYLD_LIBRARY_PATH}" ]; then
+export DYLD_LIBRARY_PATH=${DYLD_LIBRARY_PATH}:${THISDIR}/lib
+fi
+
+export PATH=${PATH}:${THISDIR}/bin
+
+export KINFITPKGINCLUDE=${THISDIR}/interface
+export KINFITPKGLIB=${THISDIR}/lib
