6408cd88dfeff3627b380dce028c8f8b38a647cd
rgerosa <>
2013-04-20 09:33:38 +0000

    Update

--- VBFHWWlnuJ/src/DataMCPlotTool.cc
+++ VBFHWWlnuJ/src/DataMCPlotTool.cc
@@ -1,20 +1,34 @@
 #include "DataMCPlotTool.h"
 
-void DrawStackError(THStack* hs,const std::string & Labels, const std::map<int,double> & SystematicErrorMap, const double & syst){ 
+void DrawStackError(THStack* hs, const std::string & Labels,  const std::map<int,double> & SystematicErrorMap,  const bool & isLog, const bool & isLabel, const double & syst){
+
   TObjArray* histos = hs->GetStack () ;
   if (histos) {
 
     Int_t number = histos->GetEntries();
     TH1F* last = (TH1F*) histos->At (number-1) ;
 
-    last->GetXaxis()->SetTitle(Labels.c_str());
-    last->GetXaxis()->SetTitleSize(0.04);
-    last->GetYaxis()->SetTitle("Entries");
+    if(isLog){ last->SetMaximum(last->GetBinContent(last->GetMaximumBin())*10000);
+               last->SetMinimum(0.001) ;
+    }
+    else { last->SetMaximum(last->GetBinContent(last->GetMaximumBin())*1.6);
+           last->SetMinimum(0) ;
+    }
+
+    if(isLabel){ last->GetXaxis()->SetTitle(Labels.c_str());
+                 last->GetXaxis()->SetTitleSize(0.04);
+                 last->GetXaxis()->SetLabelSize(0.03);
+    }
+    else {
+                 last->GetXaxis()->SetTitleSize(0.);
+                 last->GetXaxis()->SetLabelSize(0.);
+    }
+
+    last->GetYaxis()->SetTitle("entries");
     last->GetYaxis()->SetTitleSize(0.04);
-    last->SetMaximum(last->GetBinContent(last->GetMaximumBin())*1.8) ;
-    last->SetMinimum(1) ;
-
-
+    last->GetYaxis()->SetLabelSize(0.03);
+    last->GetYaxis()->SetTitleOffset(1.25);
+    last->SetFillStyle(3001);
     last->DrawClone ("hist") ;
 
     std::vector <double> vErrSys (last->GetNbinsX(),0.) ;
@@ -23,6 +37,7 @@ void DrawStackError(THStack* hs,const std::string & Labels, const std::map<int,d
     for (int i = number-2 ; i >=0 ; --i) {
       TH1F * histo = (TH1F*) histos->At (i) ;
       histo->GetXaxis()->SetTitle(Labels.c_str());
+      histo->SetFillStyle(3001);
       histo->Draw ("same hist") ;
       for(int iBin = 0 ; iBin < histo->GetNbinsX(); iBin++){
 	vErrSys.at(iBin) = vErrSys.at(iBin) + histo->GetBinContent(iBin+1)*histo->GetBinContent(iBin+1)*SystematicErrorMap.at(i)*SystematicErrorMap.at(i);
@@ -30,37 +45,51 @@ void DrawStackError(THStack* hs,const std::string & Labels, const std::map<int,d
       }
     }
     
-    Style_t origStyleLast = last->GetFillStyle ();
-    Color_t origColorLast = last->GetFillColor ();
-    last->SetFillStyle(3005);
-    last->SetFillColor(kBlack);
-    last->SetMarkerSize(0);
+    //last->SetFillStyle(3005);
+    //  last->SetFillColor(kBlack);
+    // last->SetMarkerSize(0);
     
     for (int iBin = 0 ; iBin < last->GetNbinsX(); iBin++) {
       double additionalError = sqrt(vErrSys.at(iBin) + last->GetBinContent(iBin+1) * last->GetBinContent(iBin+1) * syst * syst );
       last->SetBinError(iBin+1,sqrt(additionalError*additionalError + vErrStat.at(iBin)));
     }
     
-    last->DrawClone ("sameE2") ;
+    //    last->DrawClone ("sameE2") ;
 
   }
 }
 
 
-void DrawStackError(THStack* hs, const std::string & Labels, const TH1F*  dataHist, const std::map<int,double> & SystematicErrorMap, const double & syst){ 
+void DrawStackError(THStack* hs, const std::string & Labels, const TH1F* dataHist, const std::map<int,double> & SystematicErrorMap, const bool & isLog, const bool & isLabel, const double & syst){
+
   TObjArray* histos = hs->GetStack () ;
   if (histos) {
 
     Int_t number = histos->GetEntries();
     TH1F* last = (TH1F*) histos->At (number-1) ;
 
-    last->GetXaxis()->SetTitle(Labels.c_str());
-    last->GetXaxis()->SetTitleSize(0.04);
-    last->GetYaxis()->SetTitle("Entries");
+    if(isLog){ last->SetMaximum(std::max(last->GetBinContent(last->GetMaximumBin())*10000,dataHist->GetBinContent(dataHist->GetMaximumBin())*10000));
+               last->SetMinimum(0.001) ;
+    }
+    else { last->SetMaximum(std::max(last->GetBinContent(last->GetMaximumBin())*1.6,dataHist->GetBinContent(dataHist->GetMaximumBin())*1.6));
+           last->SetMinimum(0.) ;
+    }
+
+    if(isLabel){ last->GetXaxis()->SetTitle(Labels.c_str());
+                 last->GetXaxis()->SetTitleSize(0.04);
+                 last->GetXaxis()->SetLabelSize(0.03);
+    }
+    else {
+                 last->GetXaxis()->SetTitleSize(0.);
+                 last->GetXaxis()->SetLabelSize(0.);
+
+    }
+    last->GetYaxis()->SetTitle("entries");
     last->GetYaxis()->SetTitleSize(0.04);
-    last->SetMaximum(std::max(last->GetBinContent(last->GetMaximumBin())*1.3,dataHist->GetBinContent(dataHist->GetMaximumBin())*1.3)) ;
-    last->SetMinimum(1) ;
- 
+    last->GetYaxis()->SetLabelSize(0.03);
+    last->GetYaxis()->SetTitleOffset(1.25);
+   
+    last->SetFillStyle(3001);
     last->DrawClone ("hist") ;
 
     std::vector <double> vErrSys (last->GetNbinsX(),0.) ;
@@ -69,6 +98,7 @@ void DrawStackError(THStack* hs, const std::string & Labels, const TH1F*  dataHi
     for (int i = number-2 ; i >=0 ; --i) {
       TH1F * histo = (TH1F*) histos->At (i) ;
       histo->GetXaxis()->SetTitle(Labels.c_str());
+      histo->SetFillStyle(3001);
       histo->Draw ("same hist") ;
       for(int iBin = 0 ; iBin < histo->GetNbinsX(); iBin++){
 	vErrSys.at(iBin) = vErrSys.at(iBin) + histo->GetBinContent(iBin+1)*histo->GetBinContent(iBin+1)*SystematicErrorMap.at(i)*SystematicErrorMap.at(i);
@@ -76,40 +106,29 @@ void DrawStackError(THStack* hs, const std::string & Labels, const TH1F*  dataHi
       }
     }
     
-    Style_t origStyleLast = last->GetFillStyle ();
-    Color_t origColorLast = last->GetFillColor ();
-    last->SetFillStyle(3005);
-    last->SetFillColor(kBlack);
-    last->SetMarkerSize(0);
+
+    //    last->SetFillStyle(3005);
+    //    last->SetFillColor(kBlack);
+    //    last->SetMarkerSize(0);
     
     for (int iBin = 0 ; iBin < last->GetNbinsX(); iBin++) {
       double additionalError = sqrt(vErrSys.at(iBin) + last->GetBinContent(iBin+1) * last->GetBinContent(iBin+1) * syst * syst );
       last->SetBinError(iBin+1,sqrt(additionalError*additionalError + vErrStat.at(iBin)));
     }
     
-    last->DrawClone ("sameE2") ;
-
+    //    last->DrawClone ("sameE2") ;
   }
 }
 
-
-void LatexCMS (const double & lumi, const std::string & LeptonType){
+void LatexCMS (const double & lumi, const std::string & LeptonType, const bool & isLabel){
 
   TLatex latex;
   latex.SetNDC();
   latex.SetTextSize(0.04);
 
-  latex.SetTextAlign(31); // align right                                                                                                                        
-  latex.DrawLatex(0.69,0.962,"#sqrt{s} = 8 TeV");
-  latex.SetTextAlign(31); // align right                                                                                                                        
-  latex.DrawLatex(0.55,0.962,Form("#int #font[12]{L} dt = %.1f fb^{-1}", (float)lumi/1000));
-  latex.SetTextAlign(31); // align right                                                                                                                        
-  if(LeptonType=="Muon" || LeptonType=="muon" || LeptonType == "mu" ) latex.DrawLatex(0.80,0.962,Form("mu+jets"));
-  else  latex.DrawLatex(0.80,0.962,Form("el+jets"));
-
-  latex.SetTextAlign(11); // align left                                                                                                                         
-  //  latex.DrawLatex(0.15,0.93,"CMS,  #sqrt{s} = 7 TeV");//preliminary 2011");                                                                                 
-  latex.DrawLatex(0.15,0.962,"CMS preliminary");
+  latex.SetTextAlign(21); // align right                                                                                                                        
+  if(LeptonType == "muon" || LeptonType == "mu") latex.DrawLatex(0.4,0.962,Form("CMS preliminary, %.1f fb^{-1} at #sqrt{s} = 8 TeV, mu+jets",(float)lumi/1000));
+  else if(LeptonType == "electron" || LeptonType == "el") latex.DrawLatex(0.4,0.962,Form("CMS preliminary, %.1f fb^{-1} at #sqrt{s} = 8 TeV, el+jets",(float)lumi/1000));
 
 }
 
@@ -132,4 +151,3 @@ void SetTotalSystematicVector( std::vector<double> & SysError, THStack* hs, cons
   }
 
 }
-
