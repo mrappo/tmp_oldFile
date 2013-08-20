2bdc2fa00afe4a8dd70f00712472aac67ef7cc23
Raffaele <raffaele.gerosa@cern.ch>
2013-08-20 17:09:57 +0200

    new version

---
+++ VBFHWWlnuJ/scripts/runVBFTrees/JobHerculesMuon/bjob_13.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /afs/cern.ch/user/r/rgerosa/work/MyAnalysisVBF/VBFHWWlnuJ/scripts/runVBFTrees/
+export SCRAM_ARCH=slc5_amd64_gcc462
+cd /afs/cern.ch/work/r/rgerosa/CMSSW_5_3_3_patch3/src/ ; eval `scramv1 runtime -sh` ; cd -
+source ../setup.sh
+unbuffer .//afs/cern.ch/user/r/rgerosa/work/MyAnalysisVBF/VBFHWWlnuJ/bin/VBFNtupleFromRDTree.exe JobHerculesMuon/cfg_13.cfg >> /afs/cern.ch/user/r/rgerosa/work/MyAnalysisVBF/VBFHWWlnuJ/scripts/runVBFTrees//JobHerculesMuon/out_13.txt
