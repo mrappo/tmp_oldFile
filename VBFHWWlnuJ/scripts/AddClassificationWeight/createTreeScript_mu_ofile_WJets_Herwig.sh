f61631c25f20e9d35b4445c9b976380ad07a06c3
Raffaele <raffaele.gerosa@cern.ch>
2013-08-20 17:09:18 +0200

    new version

---
+++ VBFHWWlnuJ/scripts/AddClassificationWeight/createTreeScript_mu_ofile_WJets_Herwig.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /afs/cern.ch/work/r/rgerosa/CMSSW_5_3_3_patch3/src/
+eval `scram runtime -sh`
+cd /afs/cern.ch/work/r/rgerosa/MyAnalysisVBF/VBFHWWlnuJ
+./bin/VBFApplyMVAWeight.exe scripts/AddClassificationWeight/VBFApplyMVAWeight_BulkGraviton_ofile_WJets_Herwig.cfg
