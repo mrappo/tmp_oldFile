3e3203a554f79dc8a9471cfe7bc513667fc83455
rgerosa <raffaele.gerosa@cern.ch>
2013-11-18 11:36:38 +0100

    updates for TMVA plots

--- VBFHWWlnuJ/interface/TMVAGlob.h
+++ VBFHWWlnuJ/interface/TMVAGlob.h
@@ -28,8 +28,12 @@
 #include "TPaveText.h"
 #include "TLatex.h"
 #include "TLegend.h"
+#include "TMath.h"
+#include "TNamed.h"
+#include "TFormula.h"
 
 #include "RVersion.h"
+#include "TPaletteAxis.h"
 
 // default definition for ROC curve color, style and width                                                                                                                                
 
@@ -41,37 +45,81 @@ static std::vector<double> vec_color(color, color + sizeof(color)/sizeof(double)
 static std::vector<double> vec_linewidth(linewidth, linewidth + sizeof(linewidth)/sizeof(double));
 static std::vector<double> vec_linestyle(linestyle, linestyle + sizeof(linestyle)/sizeof(double));
 
+class significanceBox : public TObject {
+ 
+ public:
+
+  significanceBox(){
+    Signal_ = NULL ;
+    Background_ = NULL ;
+    efficiencySignal_ = NULL ;
+    efficiencyBackground_ = NULL ;  
+    significance_ = NULL;
+    line1_ = NULL;
+    line2_ = NULL ; 
+  }
+  ~significanceBox(){
+    if(Signal_!=NULL) delete Signal_;
+    if(efficiencySignal_!=NULL) delete efficiencySignal_;
+    if(Background_!=NULL) delete Background_;
+    if(efficiencyBackground_!=NULL) delete efficiencyBackground_;
+    if(significance_!=NULL) delete significance_ ;
+    if(line1_!=NULL) delete line1_ ;
+    if(line2_!=NULL) delete line2_ ;
+  } 
+
+  TH1F* Signal_;
+  TH1F* Background_;
+  TH1F* efficiencySignal_;
+  TH1F* efficiencyBackground_;
+  TH1F* significance_;  
+  TString methodName_;
+  TString methodTitle_;
+
+  TLatex* line1_ ;
+  TLatex* line2_ ;
+
+};
+
+// class for global manipulation of TMVA output + ROC, correlation, output and significance plots
+
 class TMVAGlob {
 
   public:  
-
-  TMVAGlob();
+  
+  // default constructor
+  TMVAGlob(); 
+  // default deconstructor
   ~TMVAGlob();
  
-  enum TypeOfPlot { kId = 0,
-		    kNorm,
-		    kDecorrelated,
-		    kPCA,
-		    kGaussDecorr,
-		    kNumOfMethods };
+  enum TypeOfPlot { kId = 0, kNorm, kDecorrelated, kPCA, kGaussDecorr, kNumOfMethods };
 
+  enum HistType { MVAType = 0, ProbaType = 1, RarityType = 2, CompareType = 3 };
 
+  enum SignificanceType { SoverB = 0, SoverSqrtB = 1, SoverSqrtSB = 2, Pvalue = 3};
+
+  // open one input file given the name
   void openFileInput( const TString& fin );
+  // open a set of input file and store them in the class
   void openFileInput( const std::vector<std::string> & fin );
 
+  // get back a single file from the name
   TFile* GetInputFile (const TString& fin );
+  // get back the whole set of file considered
   std::vector<TFile*> GetInputFile();
 
-  void CreateCanvasandFrame(TFile* inputFile, const double & ptMin, const double & ptMax, const std::string & outputPlotDirectory);
 
-  TKey *NextKey( TIter & keyIter, TString className);
-  int GetListOfKeys( TList& keys, TString inherits, TDirectory *dir = 0 );
-
-  void DestroyCanvases(); 
+  // create canvas and frame for the ROC plot
   void Initialize( Bool_t useTMVAStyle = kTRUE );
-  void PrintImage( TDirectory* dir = 0 , const std::string & outputPlotDirectory = "");    
+  void CreateCanvasandFrameROC(TFile* inputFile, const double & ptMin, const double & ptMax, const std::string & outputPlotDirectory);
+  void DestroyCanvases(); 
+  void PrintImageROC( TDirectory* dir = 0 , const std::string & outputPlotDirectory = "");    
+  void PrintImage   ( TCanvas* c = 0, const std::string & outputPlotDirectory = "");    
+
+
   void NormalizeHist( TH1* h );
   void NormalizeHists( TH1* sig, TH1* bkg = 0 );
+
   void GetMethodName( TString & name, TKey * mkey );
   void GetMethodTitle( TString & name, TKey * ikey );
   void GetMethodName( TString & name, TDirectory * mdir );
@@ -81,34 +129,79 @@ class TMVAGlob {
   int GetNumberOfInputVariables( TDirectory *dir );  
   int GetNumberOfInputVariablesMultiClass( TDirectory *dir );
 
-  std::vector<TString> GetInputVariableNames(TDirectory *dir );
-  std::vector<TString> GetClassNames(TDirectory *dir );
-
-  TKey* FindMethod( TString name, TDirectory *dir = 0 );
-  bool ExistMethodName( TString name, TDirectory *dir = 0 );
-
   int GetListOfMethods( TList & methods, TDirectory *dir = 0 );
   int GetListOfJobs( TFile* file, TList& jobdirs);
   int GetListOfTitles( TDirectory *rfdir, TList & titles );
   int GetListOfTitles( TString & methodName, TList & titles, TDirectory *dir = 0 );
 
+  std::vector<TString> GetInputVariableNames(TDirectory *dir );
+  std::vector<TString> GetClassNames(TDirectory *dir );
+
+  // given the iterator on the file and a class name, get the next key
+  TKey *NextKey( TIter & keyIter, TString className);
+  TKey* FindMethod( TString name, TDirectory *dir = 0 );
+
+  int  GetListOfKeys( TList& keys, TString inherits, TDirectory *dir = 0 );
+  bool ExistMethodName( TString name, TDirectory *dir = 0 );
+
   TDirectory *GetInputVariablesDir( TMVAGlob::TypeOfPlot type, TDirectory *dir = 0 );
   TDirectory *GetCorrelationPlotsDir( TMVAGlob::TypeOfPlot type, TDirectory *dir = 0 );
 
-  void plotEfficiency (TFile* inputFile, TDirectory* dir, const double & minPTbin = 200, const double & maxPTbin = 1000, const std::string & outputPlotDirectory = "");
-
   void banner4Plot (const bool & isLabel, const float & ptMin, const float & ptMax);
-
   void SetMethodName (const std::vector<std::string> & MethodName);
 
-  std::vector<TFile*> inputFiles_ ;
-  TCanvas* cROC_ ;
-  TH2F*    frameROC_ ;
-  TLegend* leg_ ;
+  // Methods for significance study
+
+  void ReadHistograms(TFile* file);
+  void SetFormula(const TString & f) { fFormula_ = f; }
+
+  TString GetFormula();
+  TString GetFormulaString() { return fFormula_; }
+  TString GetLatexFormula();
+
+  void SetSignalType    (const bool & type = false) { signalType_ = type; }
+  void SetBackgroundType(const bool & type = false) { backgroundType_ = type; }
+
+
+  // plot ROC curve
+  void plotEfficiency (TFile* inputFile, TDirectory* dir, const double & minPTbin = 200, const double & maxPTbin = 1000, const std::string & outputPlotDirectory = "");
+  // plot correlation Matrix
+  void plotCorrelationMatrix (TFile* inputFile = 0, const int & iFile = 0, const std::string & outputPlotDirectory = "");
+  // plot output distribution
+  void plotMVAs( TFile*inputFile = 0, HistType htype = MVAType, const std::string & outputPlotDirectory = "");
+  // plot signficance for different formula using or not the expected number of signal and background events
+  void plotSignificance (TFile* inputFile = 0, SignificanceType stype = SoverB, const double & numberSignalEvents = 0, const double & numberBackgroundEvents = 0,
+                         const bool & UseSignalEfficiency = false, const bool & UseBakgroundEfficiency = false, const std::string & outputPlotDirectory = "");
+
+ private:
 
   int color_index ;
   int method_index;
+  int mvas_index;
+
+  std::vector<TFile*> inputFiles_ ;
   std::vector<std::string> inputMethodName_ ;
+
+  TCanvas* cROC_ ;
+  TCanvas* cCorrelationSignal_;
+  TCanvas* cCorrelationBackground_;
+  TCanvas* cMVAs_ ;
+  TCanvas* cSignificance_ ;
+  TH2F*    frameROC_ ;
+  TLegend* legROC_ ;
+
+  TString fFormula_ ;
+  bool    signalType_ ;
+  bool    backgroundType_ ;
+
+  TList*  fInfoList_;
+
+  significanceBox* significance_ ;
+
+  TH1F* histoSignal_;
+  TH1F* histoBackground_;
+  TH1F* effBackground_;
+  TH1F* effSignal_;
 
 };
 
