b6dc860cb63d4c182656e5b0d36919e038b789ba
rgerosa <raffaele.gerosa@cern.ch>
2013-11-22 17:57:31 +0100

    news

--- VBFHWWlnuJ/interface/DataMCPlotTool.h
+++ VBFHWWlnuJ/interface/DataMCPlotTool.h
@@ -23,15 +23,15 @@
 #include "TGaxis.h"
 #include "TString.h"
 
-void DrawStackError(THStack* hs, const std::string & Labels, const TH1F* dataHist, const std::map<int,double> & SystematicErrorMap, const bool & isLog = false, const bool & isLabel = false, const double & syst = 0.044);
+void DrawStackError(THStack* hs, const std::string & Labels, const TH1F* dataHist, const std::map<int,double> & SystematicErrorMap, const bool & isLog = false, const bool & isLabel = false, const double & syst = 0.04);
 
-void DrawDoubleStackError(THStack* hs, THStack* hs_herwig, const std::string & Labels, const TH1F* dataHist, const std::map<int,double> & SystematicErrorMap, const std::map<int,double> & SystematicErrorMap_herwig, const bool & isLog = false, const bool & isLabel = false, const bool & isttbar_controlplots = false, const double & syst = 0.044);
+void DrawDoubleStackError(THStack* hs, THStack* hs_herwig, const std::string & Labels, const TH1F* dataHist, const std::map<int,double> & SystematicErrorMap, const std::map<int,double> & SystematicErrorMap_herwig, const bool & isLog = false, const bool & isLabel = false, const bool & isttbar_controlplots = false, const double & syst = 0.04);
 
-void DrawStackError(THStack* hs, const std::string & Labels,  const std::map<int,double> & SystematicErrorMap,  const bool & isLog = false, const bool & isLabel = false, const double & syst = 0.044);
+void DrawStackError(THStack* hs, const std::string & Labels,  const std::map<int,double> & SystematicErrorMap,  const bool & isLog = false, const bool & isLabel = false, const double & syst = 0.04);
 
-void DrawDoubleStackError(THStack* hs, THStack* hs_herwig, const std::string & Labels,  const std::map<int,double> & SystematicErrorMap, const std::map<int,double> & SystematicErrorMap_herwig,  const bool & isLog = false, const bool & isLabel = false,  const bool & isttbar_controlplots = false , const double & syst = 0.044);
+void DrawDoubleStackError(THStack* hs, THStack* hs_herwig, const std::string & Labels,  const std::map<int,double> & SystematicErrorMap, const std::map<int,double> & SystematicErrorMap_herwig,  const bool & isLog = false, const bool & isLabel = false,  const bool & isttbar_controlplots = false , const double & syst = 0.04);
 
-void SetTotalSystematicVector( std::vector<double> & SysError, THStack* hs, const std::map<int,double> & SystematicErrorMap, const double & syst = 0.044);
+void SetTotalSystematicVector( std::vector<double> & SysError, THStack* hs, const std::map<int,double> & SystematicErrorMap, const double & syst = 0.04);
 
 void LatexCMS (const double & lumi, const std::string & LeptonType, const bool & isLabel = false);
 
