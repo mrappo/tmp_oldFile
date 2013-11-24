b9a467d6333f5cb3ebf6ff808c682f706278a6dd
rgerosa <raffaele.gerosa@cern.ch>
2013-11-24 17:18:18 +0100

    some cleaning

--- VBFHWWlnuJ/src/ApplyMVAWeightClass.cc
+++ VBFHWWlnuJ/src/ApplyMVAWeightClass.cc
@@ -1,44 +1,47 @@
 #include "ApplyMVAWeightClass.h"
 
+// constructor from files
 ApplyMVAWeightClass::ApplyMVAWeightClass(const std::vector<TFile*> & SampleFileList, const std::string & TreeName,
                                          const std::string & inputFilePath , const std::string & Label ){
 
-  SetInputTree (SampleFileList, TreeName) ;
+  (*this).SetInputTree (SampleFileList, TreeName) ;
 
-  SetReaderTree () ;
+  (*this).SetReaderTree();
 
-  SetLabel ( Label );
+  (*this).SetLabel (Label);
 
-  SetInputFilePath ( inputFilePath ) ;
+  (*this).SetInputFilePath (inputFilePath) ;
   
   reader_ = new TMVA::Reader( TreeName_+"_"+Label_);
 
 
 }
 
+// constructor from trees
 ApplyMVAWeightClass::ApplyMVAWeightClass(const std::vector<TTree*> & SampleTreeList, const std::string & TreeName,
 		                         const std::string & inputFilePath , const std::string & Label ){
 
 
-  SetTreeName (TreeName) ;
+  (*this).SetTreeName (TreeName) ;
 
-  SetInputTree (SampleTreeList) ;
+  (*this).SetInputTree (SampleTreeList) ;
 
-  SetReaderTree (SampleTreeList) ;
+  (*this).SetReaderTree (SampleTreeList) ;
 
-  SetLabel ( Label );
+  (*this).SetLabel (Label);
 
-  SetInputFilePath ( inputFilePath ) ;
+  (*this).SetInputFilePath (inputFilePath) ;
 
   reader_ = new TMVA::Reader( TreeName_+"_"+Label_);
 
 }
 
-
-// Deconstructor                                                                                                                                                                                 
+// Deconstructor                                                                                                                                                         
 ApplyMVAWeightClass::~ApplyMVAWeightClass(){
 
-  for(size_t iTree = 0; iTree < SampleTreeList_.size() ; iTree++) { if(SampleTreeList_.at(iTree)!=0)  delete SampleTreeList_.at(iTree) ; }
+  for(size_t iTree = 0; iTree < SampleTreeList_.size() ; iTree++) { 
+   if(SampleTreeList_.at(iTree)!=0)  delete SampleTreeList_.at(iTree) ; 
+  }
 
   if(reader_!=0) reader_->Delete() ;
 
@@ -46,12 +49,11 @@ ApplyMVAWeightClass::~ApplyMVAWeightClass(){
 
 }
 
-// Set Input variables and  spectator 
-
+// Set Input variables and spectator 
 void ApplyMVAWeightClass::AddTrainingVariables ( const std::vector<std::string> & mapTrainingVariables, const std::vector<std::string> & mapSpectatorVariables){
 
-  SetTrainingVariables(mapTrainingVariables);
-  SetSpectatorVariables(mapSpectatorVariables);
+  (*this).SetTrainingVariables(mapTrainingVariables);
+  (*this).SetSpectatorVariables(mapSpectatorVariables);
 
   setTrainingVariables_  = new std::vector<Float_t> ( mapTrainingVariables_.size()) ;
   setSpectatorVariables_ = new std::vector<Float_t> ( mapSpectatorVariables_.size()) ;
@@ -64,60 +66,68 @@ void ApplyMVAWeightClass::AddTrainingVariables ( const std::vector<std::string> 
     reader_->AddSpectator(mapSpectatorVariables_.at(iVar),&setSpectatorVariables_->at(iVar));
   }
 
+  return;
 }
 
+// Prepare the tree -> set the preselection cut
 void ApplyMVAWeightClass::AddPrepareReader (const std::string & LeptonType, const std::string & preselectionCutType,
 					    std::vector<double> * JetPtBinOfTraining, const int & pTBin ){
 
   if(JetPtBinOfTraining!=NULL || !JetPtBinOfTraining ){
-
     pTJetMin_ = JetPtBinOfTraining->at(pTBin) ;
     pTJetMax_ = JetPtBinOfTraining->at(pTBin+1) ;
   }
   else{
     pTJetMin_ = 0;
     pTJetMax_ = 2000 ;
-
   }
 
   preselectionCutType_ = preselectionCutType ;
   LeptonType_ = LeptonType ;
 
+  return ;
 }
 
 
+// Book the MVA method
 void ApplyMVAWeightClass::BookMVAWeight (const std::string & methodName, const std::string & weightFile, const std::string & nameBranch) {
 
   methodName_=methodName ;
   weightFile_=weightFile ; 
   reader_->BookMVA(methodName_.c_str(),(inputFilePath_+"/"+weightFile_).c_str()) ;
   nameBranch_ = nameBranch ;  
+
+  return ;
+
 }
 
+// Fill the MVA weight
 void ApplyMVAWeightClass::FillMVAWeight (const std::string & LeptonType, const std::string & preselectionCutType) {
 
   LeptonType_ = LeptonType ;
   preselectionCutType_ = preselectionCutType ;
+
   std::cout<<std::endl;
 
+  // loop on the sample list and create the branch
   for(size_t iTree = 0; iTree < SampleTreeList_.size() ; iTree++){
 
       newBranch_  = SampleTreeList_.at(iTree)->Branch(nameBranch_.c_str(),&weight_,(nameBranch_+"/F").c_str());
+
       for( int iEntry = 0; iEntry < SampleTreeList_.at(iTree)->GetEntries(); iEntry++){
-
-
-      if(iEntry%10000 == 0 ) std::cout<<" Entry = "<<iEntry<<std::endl;
+	// loop on the entries   
+       if(iEntry%10000 == 0 ) std::cout<<" Entry = "<<iEntry<<std::endl;
       
-      SampleTreeList_.at(iTree)->GetEntry(iEntry);  weight_ = 0 ;
-
-      for( size_t iVar = 0 ; iVar < mapTrainingVariables_.size() ; iVar ++ ) 
+       SampleTreeList_.at(iTree)->GetEntry(iEntry);  weight_ = 0 ;
+       // fill the value of the training and spectator variable for each event
+       for( size_t iVar = 0 ; iVar < mapTrainingVariables_.size() ; iVar ++ ) 
 	setTrainingVariables_->at(iVar) = treeReader_.at(iTree)->getFloat(mapTrainingVariables_.at(iVar).c_str())[0] ; 
 
       for( size_t iVar = 0 ; iVar < mapSpectatorVariables_.size() ; iVar ++ ) 
 	setSpectatorVariables_->at(iVar) = treeReader_.at(iTree)->getFloat(mapSpectatorVariables_.at(iVar).c_str())[0] ;
 
+      // preselection type definition --> only the basic cut SB+SR since we want to skip only the events that are never used in the analysis
       bool isGoodEvent = false ;
-
       if (preselectionCutType_ == "basicPreselectionCutEXO" && (LeptonType_ == "Mu" || LeptonType_ == "mu" || LeptonType_ == "Muon" || LeptonType_ == "muon") ) {         
 
 	isGoodEvent = treeReader_.at(iTree)->getInt("issignal")[0] && treeReader_.at(iTree)->getFloat("v_pt")[0] > 200 && treeReader_.at(iTree)->getFloat("pfMET")[0] > 40 && 
@@ -138,26 +148,22 @@ void ApplyMVAWeightClass::FillMVAWeight (const std::string & LeptonType, const s
       }
       
       else if (preselectionCutType_ == "basicPreselectionCutHiggs" && (LeptonType_ == "Mu" || LeptonType_ == "mu" || LeptonType_ == "Muon" || LeptonType_ == "muon") ) {         
-
 	isGoodEvent = treeReader_.at(iTree)->getInt("issignal")[0] && treeReader_.at(iTree)->getFloat("v_pt")[0] > 200 && treeReader_.at(iTree)->getFloat("pfMET")[0] > 50 && 
  	              treeReader_.at(iTree)->getFloat("l_pt")[0] > 30 && treeReader_.at(iTree)->getFloat("numberJetBin")[0]<2 && 
                       ( treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] > pTJetMin_ && treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] < pTJetMax_ )  ;
       }
-      else if (preselectionCutType_ == "basicPreselectionCutHiggs" && (LeptonType_ == "El" || LeptonType_ == "el" || LeptonType_ == "Electron" || LeptonType_ == "electron") ){ 
-      
+      else if (preselectionCutType_ == "basicPreselectionCutHiggs" && (LeptonType_ == "El" || LeptonType_ == "el" || LeptonType_ == "Electron" || LeptonType_ == "electron") ){       
 	isGoodEvent = treeReader_.at(iTree)->getFloat("issignal")[0]  && treeReader_.at(iTree)->getFloat("v_pt")[0] > 200 && treeReader_.at(iTree)->getFloat("pfMET")[0] > 70 && 
 	              treeReader_.at(iTree)->getFloat("l_pt")[0] > 35 && treeReader_.at(iTree)->getFloat("numberJetBin")[0]<2 &&
                       (treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] > pTJetMin_ && treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] < pTJetMax_ )  ;
       }
-      else if (preselectionCutType_ == "basicPreselectionCutHiggs" && (LeptonType_ == "MuEl" || LeptonType_ == "muel" || LeptonType_ == "MuonEle" || LeptonType_ == "muonele") ){ 
-      
+      else if (preselectionCutType_ == "basicPreselectionCutHiggs" && (LeptonType_ == "MuEl" || LeptonType_ == "muel" || LeptonType_ == "MuonEle" || LeptonType_ == "muonele") ){       
 	isGoodEvent = treeReader_.at(iTree)->getFloat("issignal")[0]  && treeReader_.at(iTree)->getFloat("v_pt")[0] > 200 && treeReader_.at(iTree)->getFloat("pfMET")[0] > 70 && 
 	              treeReader_.at(iTree)->getFloat("l_pt")[0] > 35 && treeReader_.at(iTree)->getFloat("numberJetBin")[0]<2 &&
                       (treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] > pTJetMin_ && treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] < pTJetMax_ )  ;
       }
 
       else if (preselectionCutType_ == "basicVBFPreselectionCutHiggs" && (LeptonType_ == "Mu" || LeptonType_ == "mu" || LeptonType_ == "Muon" || LeptonType_ == "muon") ) {         
-
 	isGoodEvent = treeReader_.at(iTree)->getInt("issignal")[0] && treeReader_.at(iTree)->getFloat("v_pt")[0] > 200 && treeReader_.at(iTree)->getFloat("pfMET")[0] > 50 && 
 	              treeReader_.at(iTree)->getFloat("l_pt")[0] > 30 && treeReader_.at(iTree)->getFloat("numberJetBin")[0]>=2 && 
                       ( treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] > pTJetMin_ && treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] < pTJetMax_ )  ;
@@ -168,23 +174,29 @@ void ApplyMVAWeightClass::FillMVAWeight (const std::string & LeptonType, const s
 	              treeReader_.at(iTree)->getFloat("l_pt")[0] > 35 && treeReader_.at(iTree)->getFloat("numberJetBin")[0]>=2 &&
                       (treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] > pTJetMin_ && treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] < pTJetMax_ )  ;
       }
-      else if (preselectionCutType_ == "basicVBFPreselectionCutHiggs" && (LeptonType_ == "MuEl" || LeptonType_ == "muel" || LeptonType_ == "MuonEle" || LeptonType_ == "muonele") ){ 
-      
+      else if (preselectionCutType_ == "basicVBFPreselectionCutHiggs" && (LeptonType_ == "MuEl" || LeptonType_ == "muel" || LeptonType_ == "MuonEle" || LeptonType_ == "muonele") ){       
 	isGoodEvent = treeReader_.at(iTree)->getFloat("issignal")[0]  && treeReader_.at(iTree)->getFloat("v_pt")[0] > 200 && treeReader_.at(iTree)->getFloat("pfMET")[0] > 70 && 
 	              treeReader_.at(iTree)->getFloat("l_pt")[0] > 35 && treeReader_.at(iTree)->getFloat("numberJetBin")[0]>=2 && 
                       (treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] > pTJetMin_ && treeReader_.at(iTree)->getFloat("ungroomed_jet_pt")[0] < pTJetMax_ )  ;
       }
-      
-      if(isGoodEvent) { weight_ = reader_->EvaluateMVA(methodName_.c_str()); newBranch_->Fill() ;}
-      else { weight_ = -100. ; newBranch_->Fill() ; } 
-    } 
-      
+
+      // if is a good event -> fill the branch with the value of the output      
+      if(isGoodEvent) { weight_ = reader_->EvaluateMVA(methodName_.c_str()); 
+                        newBranch_->Fill() ;
+      }
+      else { weight_ = -100. ; 
+            newBranch_->Fill() ; 
+      } 
+    }       
     SampleTreeList_.at(iTree)->Write("", TObject::kOverwrite);
   }
+
+  return ;
+
 }
 
 
-
+// Set input tree
 void ApplyMVAWeightClass::SetInputTree (const std::vector<TFile*> & SampleFileList,  const std::string & TreeName){
 
   if(TreeName!="") TreeName_ = TreeName ;
@@ -196,30 +208,42 @@ void ApplyMVAWeightClass::SetInputTree (const std::vector<TFile*> & SampleFileLi
 
   }
 
+  return ;
+
+
 }
 
 void ApplyMVAWeightClass::SetInputTree (const std::vector<TTree*> & SampleTreeList){
 
   if(SampleTreeList.size()!=0) SampleTreeList_ = SampleTreeList ;
 
+  return ;
+
 }
 
-
+// Set Training variables name
 void ApplyMVAWeightClass::SetTrainingVariables  (const std::vector<std::string> & mapTrainingVariables){
 
   if(mapTrainingVariables.size()!=0) mapTrainingVariables_=mapTrainingVariables;
+
+  return ;
 
 }
 
 void ApplyMVAWeightClass::SetSpectatorVariables (const std::vector<std::string> & mapSpectatorVariables){
 
   if(mapSpectatorVariables.size()!=0) mapSpectatorVariables_=mapSpectatorVariables;
+ 
+  return ;
 
 }
 
+// Set input path
 void ApplyMVAWeightClass::SetInputFilePath ( const std::string & InputFilePath){
 
   if(!InputFilePath.empty()) inputFilePath_ = InputFilePath ;
+
+  return;
 
 }
 
@@ -228,6 +252,7 @@ void ApplyMVAWeightClass::SetTreeName ( const std::string & TreeName ){
   if(TreeName!="") TreeName_ = TreeName ;
   else TreeName_ = "WJet" ;
 
+  return ;
 
 }
 
@@ -235,12 +260,17 @@ void ApplyMVAWeightClass::SetLabel ( const std::string & Label ){
 
   Label_ = Label ;
 
+  return ;
+
 }
 
 void ApplyMVAWeightClass::SetReaderTree(){
 
   for(size_t iTree = 0; iTree<SampleTreeList_.size() ; iTree++)
     treeReader_.push_back(new treeReader((TTree*) SampleTreeList_.at(iTree), false) );
+
+  return ;
+
 }
 
 
@@ -249,5 +279,7 @@ void ApplyMVAWeightClass::SetReaderTree(const std::vector<TTree*> & SampleTreeLi
 
   for(size_t iTree = 0; iTree<SampleTreeList.size() ; iTree++)
     treeReader_.push_back(new treeReader((TTree*) SampleTreeList.at(iTree), false));
+
+  return ;
 
 }
